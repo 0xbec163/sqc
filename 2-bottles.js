235aff0f649736d4a7b3c7701b12ed8862cdd279
0xbec163 <kharris20@cvtc.edu>
2023-08-30 13:21:12 -0500

    Add js images

--- 2-bottles.js
+++ 2-bottles.js
@@ -2,8 +2,8 @@ const generate = function (numBottles) {
   let result = ''
 
   for (; numBottles >= 1; numBottles--) {
-    let thisNumber;
-    let nextNumber;
+    let thisNumber
+    let nextNumber
     let bottles = 'bottles'
 
     if (numBottles === 10) {
@@ -48,10 +48,10 @@ const generate = function (numBottles) {
       bottles = 'bottle'
     }
 
-    result += thisNumber + ' green ' + bottles + ' hanging on the wall,\n'
-      + thisNumber + ' green ' + bottles + ' hanging on the wall,\n'
-      + 'But if one green bottle should accidentally fall,\n'
-      + 'There\'ll be ' + nextNumber + ' green bottles hanging on the wall.\n\n'
+    result += '<p class="verse">' + thisNumber + ' green ' + bottles + ' hanging on the wall,<br>'
+      + thisNumber + ' green ' + bottles + ' hanging on the wall,<br>'
+      + 'But if one green bottle should accidentally fall,<br>'
+      + 'There\'ll be ' + nextNumber + ' green bottles hanging on the wall.</p>'
   }
   return result
 }
